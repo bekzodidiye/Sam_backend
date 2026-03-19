524850f18ea517cbdbb99cdae218fd8d499f6de2
bekzodidiye <148998731+bekzodidiye@users.noreply.github.com>
2026-03-20 01:05:04 +0500

    Delete backend directory

--- backend/api_v1/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiV1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_v1'
-
-    def ready(self):
-        import api_v1.signals
